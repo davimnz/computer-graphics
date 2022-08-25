10c96317abbae9877f06efe193fe6e9f25c7d792
vasconcelosdavi <davi.muniz41@gmail.com>
2022-08-24 22:37:05 -0300

    Add sample triangle

--- tangram/script.js
+++ tangram/script.js
@@ -1,37 +1,57 @@
-// Create an empty scene
+/* Create an empty scene */
 var scene = new THREE.Scene();
 
-var renderer = new THREE.WebGLRenderer({ antialias: true })
+/* Create a renderer */
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 
-// Create a basic perspective camera
+/* Create a basic perspective camera */
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
 
-// Configure renderer clear color
-renderer.setClearColor("#000000");
+/* Configure renderer clear color */
+renderer.setClearColor("#ffffff");
 
-// Configure renderer size
+/* Configure renderer size */
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// Append Renderer to DOM
+/* Append Renderer to DOM */
 document.body.appendChild(renderer.domElement);
 
-// Create a Cube Mesh with basic material
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: "#433F81" });
-var cube = new THREE.Mesh(geometry, material);
+/* Classes definition */
+class Triangle {
+    constructor(vertexA, vertexB, vertexC, color) {
+        this.vertexA = vertexA
+        this.vertexB = vertexB
+        this.vertexC = vertexC
+        this.color = color
 
-// Add cube to Scene
-scene.add(cube);
+        var geometry = new THREE.Geometry();
+        var vertexOne = new THREE.Vector3(this.vertexA[0], this.vertexA[1], this.vertexA[2]);
+        var vertexTwo = new THREE.Vector3(this.vertexB[0], this.vertexB[1], this.vertexC[2]);
+        var vertexThree = new THREE.Vector3(this.vertexC[0], this.vertexC[1], this.vertexC[2]);
+        var triangle = new THREE.Triangle(vertexOne, vertexTwo, vertexThree);
+        var normal = triangle.normal();
+        geometry.vertices.push(triangle.a);
+        geometry.vertices.push(triangle.b);
+        geometry.vertices.push(triangle.c);
+        geometry.faces.push(new THREE.Face3(0, 1, 2, normal));
+        this.mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: this.color }));
+    }
 
-// Render Loop
+    get getMesh() {
+        return this.mesh;
+    }
+};
+
+/* Sample triangle */
+var testTriangle = new Triangle([1, 0, 0], [0, 1, 0], [0, 0, 0], 0xff0000);
+scene.add(testTriangle.getMesh);
+
+/* Render Loop */
 var render = function () {
     requestAnimationFrame(render);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    // Render the scene
+    /* Render the scene */
     renderer.render(scene, camera);
 };
 
