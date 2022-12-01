0bcd77ec8597ffbaba96249e692170ef77d39e7b
Davi Muniz <davi.muniz41@gmail.com>
2022-12-01 10:40:58 -0300

    Space travel (#7)
    * Add gltf files
    
    * Fix gltf loading
    
    * We can see gltf now
    
    * Can move gltf now
    
    * Add camera for debug purposes
    
    * Follow ellipse trajectory
    
    * Add two cameras
    
    * Add orientation
    
    * Fix orientation
    
    * Add show / remove spaceship trajectory
    
    * Set planet
    
    * Add planet rotation
    
    * Update space-travel/readme
    
    * Update readme

---
+++ space-travel/README.md
@@ -0,0 +1,31 @@
+# Space Travel
+
+## Alunos
+
+- Alvaro Tedeschi Neto (ITA, COMP-23)
+- Davi Muniz Vasconcelos (ITA, COMP-23)
+
+## Comandos
+
+- Aperte `R` para trocar entre a câmera global e a câmera que segue a nave espacial.
+- Aperte `E` para visualizar a trajetória elíptica da nave espacial em torno do planeta vermelho.
+
+## Modelagem
+Utilizou-se o software Blender para criar os modelos 3D usados no projeto. Foram criados três modelos: uma nave espacial, um planeta vermelho e um asteroide. As Figuras 1 a 3 exibem os modelos.
+
+<p align="center">
+    <img src="figures/spaceship.PNG" alt="">
+</p>
+<p align="center">Fig. 1 - Modelagem da nave espacial no Blender.
+
+<p align="center">
+    <img src="figures/planet.PNG" alt="">
+</p>
+<p align="center">Fig. 2 - Modelagem do planeta vermelho no Blender.
+
+<p align="center">
+    <img src="figures/asteroid.PNG" alt="">
+</p>
+<p align="center">Fig. 3 - Modelagem do asteroide no Blender.
+
+## Implementação
---
+++ space-travel/script.js
@@ -0,0 +1,155 @@
+async function main() {
+    /* Create an empty scene */
+    var scene = new THREE.Scene();
+
+    /* Create a light */
+    const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 10);
+    scene.add(light);
+
+    /* Create a basic perspective camera */
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000);
+    camera.position.z = 5;
+
+    const cameraSpaceship = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000);
+    /* Create a renderer */
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+    /* Configure renderer clear color */
+    renderer.setClearColor("0x000000");
+
+    /* Configure renderer size */
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    /* Append Renderer to DOM */
+    document.body.appendChild(renderer.domElement);
+
+    /* Add mouse control */
+    mouseControls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    /* Load gltf files */
+    var loader = new THREE.GLTFLoader();
+
+    function ResizeSceneObject(sceneObj, alpha) {
+        sceneObj.scale.x *= alpha;
+        sceneObj.scale.y *= alpha;
+        sceneObj.scale.z *= alpha;
+    }
+
+    let spaceship;
+    let planet;
+    async function loadGLTF() {
+        spaceship = await loader.loadAsync('/gltf/spaceship.gltf', undefined);
+        planet = await loader.loadAsync('/gltf/planet.gltf', undefined);
+        scene.add(spaceship.scene);
+        scene.add(planet.scene);
+        ResizeSceneObject(spaceship.scene, 0.05);
+        ResizeSceneObject(planet.scene, 1);
+    }
+    loadGLTF();
+
+    /* Spaceship's trajectory */
+    const ellipseCurve = new THREE.EllipseCurve(
+        0, 0,            // ax, aY
+        5, 2,            // xRadius, yRadius
+        0, 2 * Math.PI,  // aStartAngle, aEndAngle
+        false,           // aClockwise
+        0                // aRotation
+    );
+    const totalPoints = 1000;
+    let ellipsePoints = ellipseCurve.getPoints(totalPoints);
+    ellipsePoints.forEach(p => { p.z = -p.y; p.y = 0 });
+    const ellipseGeometry = new THREE.BufferGeometry().setFromPoints(ellipsePoints);
+    const ellipseMaterial = new THREE.LineBasicMaterial({ color: "white" });
+    const ellipse = new THREE.Line(ellipseGeometry, ellipseMaterial);
+    let drawEllipse = false;
+
+    function SetSceneObjectPos(sceneObj, pos) {
+        sceneObj.position.set(pos.x, pos.y, pos.z);
+    }
+   
+    function SetSceneObjectLookAt(sceneObj, look) {
+        sceneObj.lookAt(look.x, look.y, look.z);
+    }
+
+    function SetSceneObjectRot(sceneObj, rot) {
+        sceneObj.rotation.set(rot.x, rot.y, rot.z);
+    }
+    
+    let posIdx = 0;
+    function updatePos() {
+        posIdx = (posIdx + 1) % totalPoints;
+        posIdxNext = (posIdx + 1) % totalPoints;
+        SetSceneObjectPos(spaceship.scene, ellipsePoints[posIdx]);
+        SetSceneObjectLookAt(spaceship.scene, ellipsePoints[posIdxNext]);
+        cameraSpaceship.position.copy(new THREE.Vector3(ellipsePoints[posIdx].x,
+                                                        ellipsePoints[posIdx].y,
+                                                        ellipsePoints[posIdx].z)).add(new THREE.Vector3(0, 0.05, 0.4));
+    }
+
+    const R_KEY = 114;
+    const E_KEY = 101;
+    const EnumCameraMode = {
+        SPACESHIP: 0,
+        GLOBAL: 1,
+    };
+    let CameraMode = EnumCameraMode.SPACESHIP;
+    const onKeyPress = (event) => {
+        if (event.keyCode === R_KEY) {
+            if (spaceship) {
+                if (CameraMode === EnumCameraMode.SPACESHIP)
+                    CameraMode = EnumCameraMode.GLOBAL;
+                else
+                    CameraMode = EnumCameraMode.SPACESHIP;
+            }
+        }
+        if (event.keyCode === E_KEY) {
+            drawEllipse = !drawEllipse;
+            if (drawEllipse)
+                scene.add(ellipse);
+            else
+                scene.remove(ellipse);
+        }
+    }
+    window.addEventListener('keypress', onKeyPress);
+
+    let planetRot = new THREE.Vector3(0, 0, 0);
+    let planetOmega = 0.005;
+    let planetTheta = 0;
+    var render = function () {
+        requestAnimationFrame(render);
+        mouseControls.update();
+        if (spaceship) {
+            updatePos();
+        }
+        if (planet) {
+            planetTheta += planetOmega;
+            if (planetTheta >= 2*Math.PI) {
+                planetTheta = 2*Math.PI - planetTheta;
+            }
+            planetRot.add(new THREE.Vector3(planetOmega, planetOmega, 0.5*planetOmega));
+            SetSceneObjectRot(planet.scene, planetRot);
+        }
+        switch(CameraMode) {
+            case EnumCameraMode.SPACESHIP:
+                renderer.render(scene, cameraSpaceship);
+                break;
+            case EnumCameraMode.GLOBAL:
+                renderer.render(scene, camera);
+                break;
+            default:
+                break;
+        }
+    }
+
+    render();
+}
+
+main();
